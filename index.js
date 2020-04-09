2a2568c960006b81a428536a9a0b95ffb4165695
dannyhkim <dannyhkim@github.com>
2020-04-09 17:24:03 -0400

    Connecting to DB

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 // Import routes
 const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to DB!')
+);
+
+// Middleware 
+app.use(express.json());
 
 
 // Route Middlewares
