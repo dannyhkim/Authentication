f24254df7b67d5bebbf525793c2b5e1e6b15982e
dannyhkim <dannyhkim@github.com>
2020-04-19 18:47:01 -0400

    Changes to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@ router.post('/register', async (req, res) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: hashPassword
+        password: hashPassword,
     });
     // catch error
     try{
